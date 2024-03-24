64f5face13f5eeff5fc635228a6467a3f269cc24
Minh Dang <minhdang9a8@gmail.com>
2024-03-24 18:21:43 +0700

    Complete the function to send confirmation emails for medical appointment bookings#1

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -23,43 +23,38 @@ let sendSimpleEmail = async (dataSend) => {
 
 let getBodyHTMLEmail = (dataSend) => {
     let result = '';
+    let signature = '<p style="margin-top: 20px;">Best regards,</p><p>Booking Care Team</p>';
+
     if (dataSend.language === 'vi') {
         result = `
-       <h3>Xin chào ${dataSend.patientName}!</h3>
-       <p>Bạn nhận được email này vì đã đặt lịch khám bệnh online trên booking care</p>
-       <p>Thông tin đặt lịch khám bệnh:</p>
-       <div>
-       <b>Thời gian :${dataSend.time}</b>
-       </div>
-       <div>
-       <b>Bác sĩ :${dataSend.doctorName}</b>
-       </div>
-       <p>Nếu các thông tin trên là đúng sự thật, vui lòng click vào đường link bên dưới để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh</p>
-       <div>
-         <a href=${dataSend.redirectLink} target="_blank">Click here</a>
-       </div>
-       <div>Xin chân thành cảm ơn</div>
-       `;
+            <h3>Xin chào ${dataSend.patientName}!</h3>
+            <p>Bạn nhận được email này vì đã đặt lịch khám bệnh online trên Booking Care.</p>
+            <p>Thông tin đặt lịch khám bệnh:</p>
+            <ul>
+                <li><b>Thời gian:</b> ${dataSend.time}</li>
+                <li><b>Bác sĩ:</b> ${dataSend.doctorName}</li>
+            </ul>
+            <p>Nếu các thông tin trên là đúng sự thật, vui lòng click vào đường link bên dưới để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh:</p>
+            <p><a href="${dataSend.redirectLink}" target="_blank">Click here</a></p>
+            ${signature}
+        `;
     }
 
     if (dataSend.language === 'en') {
         result = `
-          <h3>Dear ${dataSend.patientName}!</h3>
-          <p>You have received this email because you booked an online medical appointment on booking care</p>
-          <p>Information for scheduling medical examination:</p>
-          <div>
-          <b>Time:${dataSend.time}</b>
-          </div>
-          <div>
-          <b>Doctor :${dataSend.doctorName}</b>
-          </div>
-          <p>If the above information is true, please click on the link below to confirm and complete the medical appointment booking procedure.</p>
-          <div>
-            <a href=${dataSend.redirectLink} target="_blank">Click here</a>
-          </div>
-          <div>Sincerely thank</div>
-          `;
+            <h3>Dear ${dataSend.patientName}!</h3>
+            <p>You have received this email because you booked an online medical appointment on Booking Care.</p>
+            <p>Information for scheduling medical examination:</p>
+            <ul>
+                <li><b>Time:</b> ${dataSend.time}</li>
+                <li><b>Doctor:</b> ${dataSend.doctorName}</li>
+            </ul>
+            <p>If the above information is true, please click on the link below to confirm and complete the medical appointment booking procedure:</p>
+            <p><a href="${dataSend.redirectLink}" target="_blank">Click here</a></p>
+            ${signature}
+        `;
     }
+
     return result;
 };
 
